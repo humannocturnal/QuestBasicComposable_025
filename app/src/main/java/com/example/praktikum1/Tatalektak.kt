28360ba71abf96d1fd41c2485d32f18349ab4227
humannocturnal <syaputrar374@gmail.com>
2025-10-17 10:14:12 +0700

    Init project

---
+++ app/src/main/java/com/example/praktikum1/Tatalektak.kt
@@ -0,0 +1,16 @@
+package com.example.praktikum1
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)){
+    }
+
+}

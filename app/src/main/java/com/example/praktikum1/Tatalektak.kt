e3dc14ee1eec9a8c215e1303a72ec0b3aa04ccf7
humannocturnal <syaputrar374@gmail.com>
2025-10-17 12:28:20 +0700

    tambah elemen Image dengan konten notasibalok

--- app/src/main/java/com/example/praktikum1/Tatalektak.kt
+++ app/src/main/java/com/example/praktikum1/Tatalektak.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum1
 
 import android.text.Layout
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -125,7 +127,10 @@ fun TataletakBoxColumn(modifier: Modifier){
                 .height(300.dp)
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
-        ){}
+        ){
+            Image(painter = gambar,contentDescription = null,
+                contentScale = ContentScale.Fit)
+        }
     }
 }
 

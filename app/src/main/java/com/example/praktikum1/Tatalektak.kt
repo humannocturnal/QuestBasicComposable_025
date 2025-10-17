06aac12fdb4abf050fdac4474ce61d3cf992d629
humannocturnal <syaputrar374@gmail.com>
2025-10-17 12:22:32 +0700

    implementasi Spacer dan Box baru pada layout

--- app/src/main/java/com/example/praktikum1/Tatalektak.kt
+++ app/src/main/java/com/example/praktikum1/Tatalektak.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -117,7 +118,14 @@ fun TataletakBoxColumn(modifier: Modifier){
                 }
             }
         }
-
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ){}
     }
 }
 
